e38dc7ed33d88b960c1ec05bb6b6b88254041ff0
Visweshbonam <viswesh.bonam@apoorva.com>
2025-08-23 15:14:37 +0530

    ci

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "84s-remote-state-dev"
+    bucket = "practice-bucket-infra"
     key    = "roboshop-dev-bastion"
     region = "us-east-1"
     encrypt        = true
